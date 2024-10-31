7c16b0f6cf9d8b537e41166697e6d162f3f93a84
Anonymous1563 <hf277540625@gmail.com>
2024-10-31 14:50:47 +0000

    init.

--- basicsr/archs/UNet_arch.py
+++ basicsr/archs/UNet_arch.py
@@ -4,7 +4,6 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.append(parent_dir)
 
-import warnings
 import torch
 import torch.nn as nn
 import math
